fed82a57822f76b617576652661c6e1500d3b8b4
lunagv <vanessa@Diana-M.local>
2022-10-28 22:50:58 -0500

    se inicia de nuevo el componente de menu

--- src/App.js
+++ src/App.js
@@ -1,51 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 import "bootswatch/dist/lux/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
-     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarColor01">
-      <ul className="navbar-nav me-auto">
-        <li className="nav-item">
-          <a className="nav-link active" href="#">Home
-            <span class="visually-hidden">(current)</span>
-          </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Features</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Pricing</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">About</a>
-        </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-          <div className="dropdown-menu">
-            <a className="dropdown-item" href="#">Action</a>
-            <a className="dropdown-item" href="#">Another action</a>
-            <a className="dropdown-item" href="#">Something else here</a>
-            <div className="dropdown-divider"></div>
-            <a className="dropdown-item" href="#">Separated link</a>
-          </div>
-        </li>
-      </ul>
-      <form className="d-flex">
-        <input className="form-control me-sm-2" type="text" placeholder="Search"/>
-        <button className='btn btn-secondary my-2 my-sm-0' type="submit">Search</button>
-      </form>
+   <nav className='navbar navbar-expand-lg navbar-dark bg-primary'>
+    <div className='container-fluid'>
+      <a className='navbar-brand' href='inicio'> Nawi</a>
+      <div className='collapse navbar-collapse' id='navbarColor01'>
+        <ul className='navbar-nav me-auto'>
+          <li className='nav-item'>
+            <a className='nav-link active' href='home'> Home
+              <span className='visually-hidden'>(Current) </span>
+            </a>
+          </li>
+          <li className='nav-item'>
+            <a className='nav-link' href='home'>Servicios
+            </a>
+          </li>
+
+        </ul>
+
+      </div>
+
     </div>
-  </div>
-</nav>
-    </div>
+
+   </nav>
   );
 }
 
